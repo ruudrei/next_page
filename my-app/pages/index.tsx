76185a05ac2f3fb6041ff02209670daf1fcafcbd
Kazuki Noto <kazuki.noto.0206@gmail.com>
2022-08-23 06:02:11 +0000

    ファイルコミット

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <button className="btn">Button</button>
+
+    </div>
+  )
+}
+
+export default Home
